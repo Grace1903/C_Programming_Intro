4ee69e27dc0b3ac4b66c8c5629fa7a3a0d704d11
grace1903 <graceelizabethjose@gmail.com>
2023-08-11 16:07:53 +0530

    Update Exercise

--- Exercises/Set1/prime.c
+++ Exercises/Set1/prime.c
@@ -8,3 +8,30 @@
  * 7 is prime, but 9 is not prime since 9 = 3 * 3
  * 
 */
+
+#include <stdio.h>
+
+int prime(int p);
+
+int main(int argc, char const *argv[])
+{
+    for (int i = 2; i <= 200; i++)
+    {
+        if (prime(i)) {
+            printf("%d ", i);
+        }
+    }
+    
+    return 0;
+}
+
+int prime(int p)
+{
+    for(int i = 2; i < p;i++) {
+        if( p % i == 0){
+            return 0;
+        }
+     }
+
+     return 1;
+}
